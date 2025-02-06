9b1c398d4c6663a7c38547887c718d628021834b
Dykson <44515719+Dyksonn@users.noreply.github.com>
2025-02-06 17:37:42 -0300

    Add Swagger documentation support for the People API

--- Api/src/server.ts
+++ Api/src/server.ts
@@ -1,6 +1,9 @@
 import fastify from "fastify";
 import fastifyCors from '@fastify/cors'
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+
 import { createPeoples } from "./routes/create-peoples";
 import { getPeoples } from "./routes/get-peoples";
 import { putPeoples } from "./routes/put-peoples";
@@ -11,6 +14,23 @@ const app = fastify();
 app.register(fastifyCors, {
     origin: '*',
     credentials: true,
+});
+
+app.register(fastifySwagger, {
+    swagger: {
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        info: {
+            title: 'People API',
+            description: 'API to manage peoples',
+            version: '1.0.0'
+        }
+    },
+    transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
 });
 
 app.setValidatorCompiler(validatorCompiler);
